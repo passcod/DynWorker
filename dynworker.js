6107b3c9eab2e6d5921362f765f44cd481bfaead
Félix Saparelli <me@passcod.net>
2011-10-17 21:33:47 +1300

    JSify 2ee89b0

--- dynworker.js
+++ dynworker.js
@@ -1,3 +1,28 @@
-(function(){
-  // ...
-});
+(function() {
+  var DynWorker;
+  self.onmessage = function(e) {
+    return self.postMessage("Gotcha: " + e.data);
+  };
+  DynWorker = function(path) {
+    var receive, send, worker;
+    if (path == null) {
+      path = "dynworker.js";
+    }
+    worker = new Worker(path);
+    receive = function(callback) {
+      return worker.onmessage = callback;
+    };
+    send = function(msg) {
+      return worker.postMessage(msg);
+    };
+    return {
+      receive: receive,
+      send: send
+    };
+  };
+  if (typeof window === "undefined") {
+    self.DynWorker = DynWorker;
+  } else {
+    window.DynWorker = DynWorker;
+  }
+}).call(this);
