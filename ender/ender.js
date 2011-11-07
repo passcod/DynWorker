13df28913237cce781cacdec5b1214ec7cb89b17
Félix Saparelli <me@passcod.net>
2011-11-07 21:33:43 +1300

    Add Ender shortcut

---
+++ ender/ender.js
@@ -0,0 +1,10 @@
+/*!
+  * DynWorker - Web threading made easy
+  * (c) Félix Saparelli 2011
+  * https://github.com/passcod/DynWorker
+  * MIT License <http://mit.passcod.net>
+  */
+
+!function ($) {
+  $.ender({ worker: DynWorker });
+}(ender);
