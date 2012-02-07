911efad0b48fc53835bb93c28ad5be2d5bd3a4ef
Felix Saparelli <me@passcod.net>
2012-02-08 03:39:24 +1300

    Complete rewrite. Most of orig functionality is there

--- README.markdown
+++ README.markdown
@@ -1,91 +1,133 @@
 DynWorker
 =========
 
+**DynWorker** is a lowish-level library designed to make threading fun.
 
-The Talk
---------
+## The Five Pillars
 
-DynWorker is a WebWorker library which makes it easier to work with workers (haha).
-You no longer need to create a separate file for each different worker... the only
-extra file you will ever have to load is `dynworker.js`.
+    DynWorker.file = '/dynworker.min.js';
+    var thread = new DynWorker;
 
-DynWorker contains a few utilities to augment your worker. You can easily inject
-functions into the worker, run arbitrary code, pass messages containing mixed
-data (everything is JSON-encoded), and access DOM storage (local and session).
+You do not need to specify a file anymore. You specify once which file
+contains **DynWorker**, and we use that. A requirement, however, is that
+this file should not be concatenated unless you know exactly what you're doing.
 
-Future developments may include being able to clone workers, access the DOM asynchronously,
-make full XHR requests (including XML parsing), access the indexedDB directly from
-the worker, and/or (un)serialize a worker.
+---
 
+    thread.destroy();
+    thread.destroy(true);
 
-The Code
---------
+You can destroy a Worker at any time. The first version will tell the worker
+to commit seppuku, while the second just guillotines them on-the-spot.
 
-```javascript
-// If the library is in the current dir and
-// is named dynworker.js, this works:
-var worker = new DynWorker();
+---
 
-// Otherwise you need to specify a filename,
-// but it needs to be on the same domain.
-var worker = new DynWorker("/js/lib/dynworker.min.js");
+    thread.push('name', ['data']);
 
-// You can have a shortcut with Ender:
-var worker = new $.worker();
+You can `push` data into a worker. This will store the data directly onto
+the worker's global object, so you might want to do some namespacing on the
+name (e.g. `'ns.name'`). Or not &ndash; you decide. The data can be any of:
 
-// You can also modify the default path once and for all:
-DynWorker.path("path/to/dynworker.js");
-var worker = new DynWorker();
+  + String
+  + Number
+  + Boolean
+  + Array
+  + Object / Hashmap
+  + Function
 
+It cannot (yet) be an object or array containing functions.
 
-// The function is namespaced under $.ns
-// inside the worker.
-worker.inject("funcName", function(arg1, arg2) {
-  var result = "Do something awesome here";
-  
-  // In-worker helpers are namespaced under $
-  $.receive(function(e, data) {
-    // Receive messages from the main thread
-  });
-  
-  return result;
-});
+---
 
-// The callback gets back the raw event and the
-// parsed data
-worker.receive(function(e, data) {
-  data; // Display and strike awe
-});
+    thread.pull('name', function (data) {});
 
+You can `pull` a variable from the worker at any time. This is the inverse of
+`push`, so the above restrictions apply.
 
-// The #run function wraps the function call in a
-// $.send() so the return value of the
-// function is sent up.
-worker.run("funcName", arg1, arg2);
+---
 
-// Hence, these two are equivalent:
-worker.run("funcName");
-worker.eval("$.send($.ns['funcName']());");
-```
+    thread.listen(function (msg) {});
 
-### DOM storage
+You can listen for messages from the worker. All internal messages are filtered
+out, so you only get the good stuff.
 
-The `$.localStorage` API mimics the `window.localStorage` API, minus
-the array-like interface. Under the hood, all calls are asynchronous, but it
-doesn't matter too much. All workers and the main thread use the same DOM
-storage. The `$.sessionStorage` API is the same.
+---
 
-Inside the worker:
+    thread.run('func_name', args...);
 
-```javascript
-$.localStorage.setItem("key", "data");
-$.localStorage.getItem("key", function(data) {
-  // Do something with that data
-});
-```
+You can easily `run` a function in the worker and pass arguments. The arguments
+are also restricted by the list above.
+
+---
+
+    thread.eval('code');
+
+You can `eval` code in the worker.
+
+---
+
+All these methods are also available from within a worker on the special
+`parent_thread` global. The reverse the roles and allow data and code to be pushed,
+pulled, ran, and evaluated in the parent thread _from the worker_. (This is
+potentially very dangerous if you allow unchecked code to run within a worker.
+So don't.)
 
 
-The License
------------
+## You, you, and you
 
-DynWorker is licensed under this [MIT License](http://mit.passcod.net).
+Manipulating a single worker is cool, but we can do better. There's now a
+special `Workers` object. Watch:
+
+    var three_little_mice = Workers(thread1, thread2, thread3);
+    three_little_mice.push('name', ['data']);
+
+Okay, so we can take workers, group them, and execute stuff on all of them
+at once. But, wait, there's more:
+
+    Workers(thread1, thread2).addClass('furry');
+    Workers('.furry').pull('tail', callback);
+
+What's that? It looks like CSS! Well, you can add classes to workers and
+use the class to select them. How is it different than the previous,
+variable-based way? Well, you can add or remove workers in one part of your
+code and use them in another, for one. Awesome, right?
+
+    Workers().create(6).addClass('furry');
+
+Oh wow: we've selected an empty set, created six workers, and gave them the
+class `furry`. Similarly, you can `add` existing workers to a set, `remove`
+matching workers, or even `destroy` them:
+
+    // Add...
+    Workers('.furry').add(a_thread)...
+    Workers('.furry').add('.mammals')...
+    
+    // Remove...
+    Workers('.furry').remove(a_thread)...
+    Workers('.furry').remove('.rodents')...
+    
+    // This is a mice trap:
+    Workers('.furry.rodents').destroy();
+
+
+## We are Legion
+
+Two more goodies (will, soon) make stuff even more interesting:
+
+    Workers.pub('channel', data);
+    Workers.sub('channel', function (data) {});
+
+Pub/Sub for threading! What will they think of next?
+
+
+Me & Thee
+=========
+
+Hiya! I'm Félix Saparelli, also known as [passcod](http://passcod.net). I lurk
+in the streets of Auckland (and some other places), New Zealand, as well as on
+[Twitter](https://twitter.com/passcod). I land some cool Javascript and Ruby
+stuff &ndash; and some crappy things, too :)
+
+You are just passing, but if you've read until the very end, then I'm pretty
+sure I like you. So I'll give you all this, for
+[free (which is written Em-Aye-Tee)](http://passcod.mit-license.org).
--- dynworker.js
+++ dynworker.js
@@ -1,196 +1,263 @@
 (function() {
-  /*
-  DynWorker is copyright Félix "passcod" Saparelli
-   and licensed under MIT: http://mit.passcod.net
-  */
   "use strict";
-  var DynWorker, InWorker;
-  var __slice = Array.prototype.slice;
-  DynWorker = function(path) {
-    var cmd, inject, log, receive, run, send, store, weval, worker;
-    if (path == null) {
-      path = DynWorker.libpath;
-    }
-    worker = new Worker(path);
-    receive = function(func) {
-      var callback;
-      callback = function(e) {
-        return func(e, JSON.parse(e.data));
-      };
-      return worker.addEventListener("message", callback, false);
-    };
-    log = function() {
-      return receive(function(e, data) {
-        return console.log(data);
-      });
-    };
-    send = function(msg) {
-      return worker.postMessage(JSON.stringify(msg));
-    };
-    cmd = function() {
-      var action, arg, args, msg, objAdd, _i, _len;
-      action = arguments[0], args = 2 <= arguments.length ? __slice.call(arguments, 1) : [];
-      msg = {
-        DynWorkerAction: action,
-        args: 0
-      };
-      objAdd = function(arg) {
-        msg.args++;
-        return msg["arg" + msg.args] = arg;
-      };
-      for (_i = 0, _len = args.length; _i < _len; _i++) {
-        arg = args[_i];
-        objAdd(arg);
-      }
-      return send(msg);
-    };
-    weval = function(code) {
-      return cmd('eval', code);
-    };
-    inject = function(name, func) {
-      var sfunc;
-      sfunc = func.toString();
-      if (!/^function \(/.test(sfunc)) {
-        sfunc.replace(/^function [^\(]+\(/, 'function (');
-      }
-      return weval("$.ns['" + name + "']=" + sfunc + ";");
-    };
-    run = function() {
-      var args, name;
-      name = arguments[0], args = 2 <= arguments.length ? __slice.call(arguments, 1) : [];
-      return weval("$.send($.ns['" + name + "'].apply(null, " + (JSON.stringify(args)) + "));");
-    };
-    store = function(type, action, key, data) {
-      var val;
-      switch (action) {
-        case 'key':
-          val = type === 'local' ? window.localStorage.key(key) : window.sessionStorage.key(key);
-          return cmd('callback', data, [val]);
-        case 'get':
-          val = type === 'local' ? window.localStorage.getItem(key) : window.sessionStorage.getItem(key);
-          return cmd('callback', data, [val]);
-        case 'set':
-          if (type === 'local') {
-            return window.localStorage.setItem(key, data);
-          } else {
-            return window.sessionStorage.setItem(key, data);
-          }
-          break;
-        case 'remove':
-          if (type === 'local') {
-            return window.localStorage.removeItem(key);
-          } else {
-            return window.sessionStorage.removeItem(key);
-          }
-          break;
-        case 'clear':
-          if (type === 'local') {
-            return window.localStorage.clear();
-          } else {
-            return window.sessionStorage.clear();
-          }
-      }
-    };
-    receive(function(e, data) {
-      switch (data['DynWorkerAction']) {
-        case 'domstorage':
-          return store(data.arg1, data.arg2, data.arg3, data.arg4);
-      }
-    });
-    return {
-      receive: receive,
-      log: log,
-      send: send,
-      cmd: cmd,
-      eval: weval,
-      inject: inject,
-      run: run
-    };
-  };
-  DynWorker.libpath = "dynworker.js";
-  DynWorker.path = function(path) {
-    if (path) {
-      return DynWorker.libpath = path;
-    }
-  };
-  InWorker = function() {
-    var callbacks, cmd, deepCallback, receive, send, store;
-    callbacks = [];
-    deepCallback = function(index, arguments_array) {
-      return callbacks[index].apply(null, arguments_array);
-    };
-    receive = function(func) {
-      var callback;
-      callback = function(e) {
-        return func(e, JSON.parse(e.data));
-      };
-      return self.addEventListener("message", callback, false);
-    };
-    send = function(msg) {
-      return self.postMessage(JSON.stringify(msg));
-    };
-    cmd = function() {
-      var action, arg, args, msg, objAdd, _i, _len;
-      action = arguments[0], args = 2 <= arguments.length ? __slice.call(arguments, 1) : [];
-      msg = {
-        DynWorkerAction: action,
-        args: 0
-      };
-      objAdd = function(arg) {
-        msg.args++;
-        return msg["arg" + msg.args] = arg;
-      };
-      for (_i = 0, _len = args.length; _i < _len; _i++) {
-        arg = args[_i];
-        objAdd(arg);
-      }
-      return send(msg);
-    };
-    store = function(scope) {
-      return {
-        key: function(index, callback) {
-          var i;
-          i = callbacks.push(callback);
-          return cmd('domstorage', scope, 'key', index, i - 1);
-        },
-        getItem: function(key, callback) {
-          var i;
-          i = callbacks.push(callback);
-          return cmd('domstorage', scope, 'get', key, i - 1);
-        },
-        setItem: function(key, data) {
-          return cmd('domstorage', scope, 'set', key, data);
-        },
-        removeItem: function(key) {
-          return cmd('domstorage', scope, 'remove', key);
-        },
-        clear: function() {
-          return cmd('domstorage', scope, 'clear');
-        }
-      };
-    };
-    return {
-      ns: {},
-      receive: receive,
-      send: send,
-      cmd: cmd,
-      callback: deepCallback,
-      localStorage: store('local'),
-      sessionStorage: store('session')
-    };
-  };
-  if (typeof window === "undefined") {
-    self.DynWorker = DynWorker;
-    self.$ = new InWorker();
-    $.receive(function(e, data) {
-      switch (data['DynWorkerAction']) {
-        case 'eval':
-          return eval(data.arg1);
-        case 'callback':
-          return $.callback(data.arg1, data.arg2);
-      }
-    });
+  var global, DynWorker, Workers, Parent;
+  
+  /* We should not care if we're in a worker or in a parent
+   * thread, as children are allowed according to the spec,
+   * if not all implementations. So all the tools should be
+   * available, to the exception of the `parent` variable in
+   * the main thread.
+   */
+  if (typeof window !== "undefined") {
+    global = window;
   } else {
-    window.DynWorker = DynWorker;
+    global = self;
+    
+    self.addEventListener("message", function (event) {
+      var msg = event.data;
+      
+      if (/^DynWorker\:/.test(msg)) {
+        msg = msg.replace(/^DynWorker\:/, '');
+        
+        if (/^eval\:/.test(msg)) {
+          msg = msg.replace(/^eval\:/, '');
+          eval.call(self, JSON.parse(msg));
+        }
+        
+        if (/^data\:/.test(msg)) {
+          msg = JSON.parse(msg.replace(/^data\:/, ''));
+          self[msg.name] = msg.data;
+        }
+        
+        if (/^function\:/.test(msg)) {
+          msg = JSON.parse(msg.replace(/^function\:/, ''));
+          eval.call(self, "self."+msg.name+"="+msg.source);
+        }
+        
+        if (/^pull\:/.test(msg)) {
+          msg = JSON.parse(msg.replace(/^pull\:/, ''));
+          eval.call(self, "self.postMessage('DynWorker:callback:'+JSON.stringify({callback:"+msg.callback+",args:[self."+msg.name+"]}));");
+        }
+      }
+    }, false);
+  }
+  
+  Workers = function () {
+    var i, selection;
+    
+    /* The selection is originally empty. If no arguments are
+     * passed, the foreach loop does not run, and it will
+     * remain empty.
+     */
+    selection = [];
+    
+    for (i in arguments) {
+      if (typeof arguments[i] === "string") {
+        selection.push.apply(selection, select(arguments[i]));
+      } else if (arguments[i] instanceof DynWorker) {
+        selection.push(arguments[i]);
+      }
+    }
+    
+    return new Workers.wrap(selection);
+    
+    /* Go through the Workers.all array and return the
+     * workers that match the given selector.
+     * 
+     * Currently supports:
+     *  - `*`: get all workers
+     *  - `.class`: get all workers that have the class
+     */
+    function select(selector) {
+      if (selector === "*") {return Workers.all;}
+      var i, selection = [];
+      selector = selector.replace(/^\./, '');
+      for (i in Workers.all) {
+        if (Workers.all[i].hasClass(selector)) {
+          selection.push(Workers.all[i]);
+        }
+      }
+      return selection;
+    }
+  };
+  
+  Workers.all = [];
+  
+  Workers.wrap = function (selection) {this.selection = selection;}
+  
+  Workers.wrap.prototype.create = function (n) {
+    for (var i = 0; i < n; i += 1) {
+      this.selection.push(new DynWorker());
+    }
+    return this;
+  };
+  
+  Workers.wrap.prototype.addClass = function (klass) {
+    for (var i in this.selection) {
+      this.selection[i].addClass(klass);
+    }
+    return this;
+  };
+  
+  Workers.wrap.prototype.removeClass = function (klass) {
+    for (var i in this.selection) {
+      this.selection[i].removeClass(klass);
+    }
+    return this;
+  };
+  
+  Workers.wrap.prototype.push = function (name, data) {
+    for (var i in this.selection) {
+      this.selection[i].push(name, data);
+    }
+    return this;
+  };
+  
+  Workers.wrap.prototype.pull = function (name, callback) {
+    for (var i in this.selection) {
+      this.selection[i].pull(name, callback);
+    }
+    return this;
+  };
+  
+  Workers.wrap.prototype.eval = function (code) {
+    for (var i in this.selection) {
+      this.selection[i].eval(code);
+    }
+    return this;
+  };
+  
+  Workers.wrap.prototype.destroy = function (kill) {
+    for (var i in this.selection) {
+      this.selection[i].destroy(kill);
+    }
+    return this;
+  };
+  
+  Workers.wrap.prototype.get = function (n) {
+    return n? this.selection[n] : this.selection[0];
+  };
+  
+  DynWorker = function () {
+    var worker, classes, listeners,
+        weval, listen, callbacks,
+        addClass, removeClass, hasClass,
+        push, pull,
+        seppuku, guillotine;
+    
+    worker = new Worker(DynWorker.file);
+    worker.postMessage("DynWorker:start");
+    classes = [];
+    listeners = [];
+    callbacks = [];
+    
+    weval = function (code) {
+      worker.postMessage("DynWorker:eval:"+JSON.stringify(code));
+    };
+    
+    worker.addEventListener("message", function (event) {
+      var i, msg = event.data;
+      if (/^DynWorker\:/.test(msg)) {
+        msg = msg.replace(/^DynWorker\:/, '');
+        if (/^callback\:/.test(msg)) {
+          msg = JSON.parse(msg.replace(/^callback\:/, ''));
+          var callback = callbacks[msg.callback];
+          callback.apply({}, msg.args);
+          callbacks[msg.callback] = undefined;
+        }
+      } else {
+        for (i in listeners) {
+          listeners[i](msg);
+        }
+      }
+    }, false);
+    
+    addClass = function (klass) {
+      if (classes.indexOf(klass) === -1) {
+        classes.push(klass);
+      }
+    };
+    
+    removeClass = function (klass) {
+      if (classes.indexOf(klass) !== -1) {
+        classes.splice(classes.indexOf(klass), 1);
+      }
+    };
+    
+    hasClass = function (klass) {
+      return classes.indexOf(klass) !== -1;
+    };
+    
+    
+    push = function (name, data) {
+      if (typeof data === "function") {
+        worker.postMessage("DynWorker:function:"+JSON.stringify({
+          name: name,
+          source: data.toString()
+        }));
+      } else {
+        worker.postMessage("DynWorker:data:"+JSON.stringify({
+          name: name,
+          data: data
+        }));
+      }
+    };
+    
+    pull = function (name, callback) {
+      worker.postMessage("DynWorker:pull:"+JSON.stringify({
+        callback: callbacks.push(callback)-1,
+        name: name
+      }));
+    };
+    
+    seppuku = function () {
+      weval("self.close()");
+    };
+    
+    guillotine = function () {
+      worker.terminate();
+    };
+    
+    
+    this.addClass = addClass;
+    this.removeClass = removeClass;
+    this.hasClass = hasClass;
+    
+    this.push = push;
+    this.pull = pull;
+    this.destroy = function (kill) {
+      if (kill) {
+        guillotine();
+      } else {
+        seppuku();
+      }
+      
+      Workers.all.splice(Workers.all.indexOf(this), 1);
+      worker = null;
+    };
+    
+    this.eval = weval;
+    this.listen = function (callback) {
+      listeners.push(callback);
+    };
+    
+    Workers.all.push(this);
+  };
+  
+  DynWorker.file = 'dynworker.js';
+  
+  
+  Parent = function () {
+    return {
+      log: function () { return 'Hey'; }
+    };
+  };
+  
+  /* Add all our stuff to the global */
+  global.Workers = Workers;
+  global.DynWorker = DynWorker;
+  
+  if (typeof window === "undefined") {
+    global.parent_thread = new Parent();
   }
 }).call(this);
